abfc5a4fc67ff1007926ebfb6af817f230161438
Halil Karakose <halil.karakose@kodfarki.com>
2017-07-26 09:49:02 +0300

    initial commit

---
+++ hello-maven/src/main/java/com/kodfarki/maven/hello/HelloMaven.java
@@ -0,0 +1,12 @@
+package com.kodfarki.maven.hello;
+
+/**
+ * User: Halil Karakose
+ * Date: 26/07/2017
+ * Time: 09:15
+ */
+public class HelloMaven {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven");
+    }
+}
